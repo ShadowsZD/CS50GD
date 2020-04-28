8ad425e1fe861f080558454b8eda203e139efb98
ShadowsZD <kartkork@gmail.com>
2020-04-27 23:30:43 -0300

    Classes and collision

--- Pong/main.lua
+++ Pong/main.lua
@@ -1,4 +1,7 @@
 push = require 'push' --lib
+Class = require 'class'
+require 'Paddle'
+require 'Ball'
 
 WINDOW_WIDHT = 1280
 WINDOW_HEIGHT = 720
@@ -13,6 +16,7 @@ PADDLE_SPEED = 200
 function love.load()
     --uncomment to test stuff
     love.graphics.setDefaultFilter('nearest', 'nearest')
+    love.window.setTitle('Pong')
     math.randomseed(os.time())
     --smallFont = love.graphics.newFont('font.ttf', 8)
     --love.graphics.setFont(smallFont)
@@ -25,37 +29,76 @@ function love.load()
 
     scorePlayer1 = 0
     scorePlayer2 = 0
+    --put score in player class
+    player1 = Paddle(10, 30, 5, 20)
+    player2 = Paddle(VIRTUAL_WIDTH - 10, VIRTUAL_HEIGHT - 30, 5, 20)
 
-    player1Y = 30
-    player2Y = VIRTUAL_HEIGHT - 50
-
-    ballX = VIRTUAL_WIDTH / 2 - 2
-    ballY = VIRTUAL_HEIGHT / 2 - 2
-    ballDX = math.random(2) == 1 and 100 or -100
-    ballDY = math.random(-50, 50)
+    --place ball in the middle of screen
+    ball = Ball(VIRTUAL_WIDTH / 2 - 2, VIRTUAL_HEIGHT / 2 - 2, 4, 4)
 
     gameState = 'start'
 end
 
 function love.update(dt)
+    if gameState == 'play' then
+        if ball:collides(player1) then
+            ball.dx = -ball.dx * 1.03
+            ball.x = player1.x + 5
+
+            if ball.dy < 0 then
+                ball.dy = -math.random(10, 150)
+            else 
+                ball.dy = math.random(10, 150)
+            end
+        end
+        
+        if ball:collides(player2) then
+            ball.dx = -ball.dx * 1.03
+            ball.x = player2.x - 4
+
+            if ball.dy < 0 then
+                ball.dy = -math.random(10, 150)
+            else 
+                ball.dy = math.random(10, 150)
+            end
+        end
+
+        --detect lower bound collision
+        if ball.y <= 0 then
+            ball.y = 0
+            ball.dy = -ball.dy
+        end
+
+        if ball.y >= VIRTUAL_HEIGHT - 4 then
+            ball.y = VIRTUAL_HEIGHT - 4
+            ball.dy = -ball.dy
+        end
+    end
+
     --Player 1 movement
     if love.keyboard.isDown('w') then
-        player1Y = math.max(0, player1Y + (PADDLE_SPEED * -1) * dt) --cannot pass end of screen
+        player1.dy = -PADDLE_SPEED
     elseif love.keyboard.isDown('s') then
-        player1Y = math.min(VIRTUAL_HEIGHT - 20 , player1Y + PADDLE_SPEED * dt)
+        player1.dy = PADDLE_SPEED
+    else
+        player1.dy = 0
     end
 
     --Player 2 movement
     if love.keyboard.isDown('up') then
-        player2Y = math.max(0, player2Y + (PADDLE_SPEED * -1) * dt)
+        player2.dy = -PADDLE_SPEED
     elseif love.keyboard.isDown('down') then
-        player2Y = math.min(VIRTUAL_HEIGHT - 20, player2Y + PADDLE_SPEED * dt)
+        player2.dy = PADDLE_SPEED
+    else 
+        player2.dy = 0
     end
 
     if gameState == 'play' then
-        ballX = ballX + ballDX * dt
-        ballY = ballY + ballDY * dt
+        ball:update(dt)
     end
+
+    player1:update(dt)
+    player2:update(dt)
 end 
 
 function love.keypressed(key)
@@ -67,11 +110,7 @@ function love.keypressed(key)
         else
             gameState = 'start'
 
-            ballX = VIRTUAL_WIDTH / 2 - 2
-            ballY = VIRTUAL_HEIGHT / 2 - 2
-
-            ballDX = math.random(2) == 1 and 100 or -100
-            ballDY = math.random(-50, 50) * 1.5
+            ball:reset()
         end
     end
 end
@@ -87,8 +126,13 @@ function love.draw()
         'center')               --alignment mode (center,left,right)
     love.graphics.print(tostring(scorePlayer1), VIRTUAL_WIDTH / 2 - 50, VIRTUAL_HEIGHT/3)
     love.graphics.print(tostring(scorePlayer2), VIRTUAL_WIDTH / 2 + 30, VIRTUAL_HEIGHT/3)
-    love.graphics.rectangle('fill', 10, player1Y, 5, 20)
-    love.graphics.rectangle('fill', VIRTUAL_WIDTH - 10, player2Y, 5, 20)
-    love.graphics.rectangle('fill', ballX, ballY, 4, 4)
+    player1:render()
+    player2:render()
+    ball:render()
     push:apply('end')
+end
+
+function displayFPS()
+    love.graphics.setColor(0, 255, 0, 255)
+    love.graphics.print('FPS: ' .. tostring(love.timer.getFPS()), 10, 10)
 end
